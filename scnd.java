a23752df2b83743d15b85ca4da42274adba79b0f
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-03-21 15:00:35 +0530

    Add files via upload

---
+++ scnd.java
@@ -0,0 +1,11 @@
+class Scnd{
+  public static void main(String args[])
+{
+int num;
+num=100;
+System.out.println("this is num"+num);
+num=num*2;
+System.out.print("the value of number *2 is: ");
+System.out.println(num);
+}
+}
